c7be505d7f179cb966b1c46bb8a5cb0c9b1ec245
OliHoward2 <oli.howard@icloud.com>
2016-04-19 18:05:45 +0100

    Update eventManager.py

--- eventManager.py
+++ eventManager.py
@@ -16,14 +16,8 @@ class EventManager:
             event = datetime.strptime(date, '%b %d %Y %I:%M%p')
             for i in file:
                 if name == key:
-                    value.add(event)
+                    value.append(event)
                     
-        def setDate(name, date):
-            with open('schedules.txt') as json_file:
-                json_data = json.load(json_file)
-            name = jason_data[name']
-            name
-            
         def deleteDate(name, date):
             file = open('schedules.txt', 'w')
             event = datetime.strptime(date, '%b %d %Y %I:%M%p')
