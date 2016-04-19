8e986874083d5d488e9ed5b16189e972f0870755
OliHoward2 <oli.howard@icloud.com>
2016-04-19 18:14:08 +0100

    Update eventManager.py

--- eventManager.py
+++ eventManager.py
@@ -10,20 +10,21 @@ class EventManager:
         
         namesList = self.schedules.getAllStudents()
     
-        def addDate(name, date):
+        def addEvent(name, date):
             file = open('schedules.txt', 'w')
             #date as string in format 'April 17 2016 10:00PM'
             event = datetime.strptime(date, '%b %d %Y %I:%M%p')
             for i in file:
-                if name == key:
-                    value.append(event)
-                    
-        def deleteDate(name, date):
+                if i == name:
+                    schedules[name].append(event)
+                
+        def removeGarment(name, date):
             file = open('schedules.txt', 'w')
             event = datetime.strptime(date, '%b %d %Y %I:%M%p')
             for i in file:
-                if name == key:
-                    if value[i] = event:
-                        value.remove(event) 
+                if i == name:
+                    for i in schedules[name]:
+                        if i == event:
+                            schedules[name].remove[i]
 
 main = Main()
