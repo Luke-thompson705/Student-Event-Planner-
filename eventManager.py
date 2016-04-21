efeb7fcafcd3079b93c48ad409da130a01254361
OliHoward2 <oli.howard@icloud.com>
2016-04-21 18:40:39 +0100

    Update eventManager.py

--- eventManager.py
+++ eventManager.py
@@ -10,25 +10,23 @@ class EventManager:
         
         namesList = self.schedules.getAllStudents()
     
-        def addEvent(name, date):
-            #date as string in format 'April 17 2016 10:00PM'
-            event = datetime.strptime(date, '%B %d %Y %I:%M%p')
+                def addEvent(name, date, description):
+            event = {date: description}
             with open("schedules.txt") as dataFile:
                 schedules = json.load(dataFile)
                 schedules[name].append(event)
             with open("schedules.txt", "w") as newFile:
                 json.dump(schedules, newFile)
                         
-        def removeEvent(name, date):
-            evemt = datetime.strptime(date, '%B %d %Y %I:%M%p')
+        def removeEvent(date):
             with open("schedules.txt") as dataFile:
                 schedules = json.load(dataFile)
                 for i in schedules:
-                    if i == name:
-                        for j in name:
-                            if j == event:
-                                schedules[name].remove(j)
+                    for j in schedules[i]:
+                        if j == date:
+                            schedules[i].remove(j)
             with open("schedules.txt", "w") as newFile:
                 json.dump(schedules, newFile)
+
 
 main = Main()
